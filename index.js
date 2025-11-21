d0fe424fada889259b42954f5feb8f3c72a24562
Steven Ouyang <houyang@ucsd.edu>
2025-11-20 23:58:20 -0800

    lab 8 (2)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (profileStats) {
   profileStats.innerHTML = `
         <h2>My GitHub Stats</h2>
         <dl>
-        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
           <dt>Following:</dt><dd>${githubData.following}</dd>
           <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
           <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
